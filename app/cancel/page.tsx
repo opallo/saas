e66fdcc71590cbc2433e3c3740bbd99eb44a8a71
opallo <opallo.collins@gmail.com>
2024-10-15 03:36:29 -0700

    stripe checkout workin locally

---
+++ app/cancel/page.tsx
@@ -0,0 +1,9 @@
+// app/cancel/page.tsx
+export default function CancelPage() {
+  return (
+    <div>
+      <h1>Payment Canceled</h1>
+      <p>Your payment was not completed. Feel free to try again later.</p>
+    </div>
+  );
+}
---
+++ app/success/page.tsx
@@ -0,0 +1,9 @@
+// app/success/page.tsx
+export default function SuccessPage() {
+  return (
+    <div>
+      <h1>Thank you for your payment!</h1>
+      <p>Your payment was successful. We’ll get started on your order shortly.</p>
+    </div>
+  );
+}
