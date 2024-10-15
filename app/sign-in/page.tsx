1a99cc4c4decde5d4df9f72fa7aa5789c28ac104
opallo <opallo.collins@gmail.com>
2024-10-15 03:38:00 -0700

    remove unused dependency

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,5 +1,4 @@
 import { SignIn } from "@clerk/nextjs";
-import Header from "components/Header";
 export default function SignInPage() {
   return (
     <div className="flex justify-center items-center min-h-screen">
