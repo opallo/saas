89a155c9a1cc6d8938639632ef7303b4a9f67798
opallo <opallo.collins@gmail.com>
2024-10-20 20:52:08 -0700

    merge dev-new

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,25 @@
 'use client';
 import Header from 'components/Header';
 import CheckoutButton from 'components/CheckoutButton';
+import { useUser } from '@clerk/nextjs'
 
 export default function HomePage() {
+  const { isLoaded, isSignedIn, user } = useUser()
+  if (!isLoaded || !isSignedIn) {
+    return null
+  }
+
   return (
     <div>
       <Header />
+      <div>Hello, {user.firstName} ! welcome to Clerk</div>
+      {user.emailAddresses.map((email) => (
+        <div key={email.emailAddress}>{email.emailAddress}</div>
+      ))}
+      {user.id}
       <CheckoutButton />
     </div>
   );
 }
+
+
