6bfd86551db20461f7c5f890bfeef3a4227c086b
opallo <opallo.collins@gmail.com>
2024-10-20 21:30:11 -0700

    clean up clerk webhooks and supabase. prep for jwt db authenticaiton

--- app/page.tsx
+++ app/page.tsx
@@ -2,25 +2,12 @@
 'use client';
 import Header from 'components/Header';
 import CheckoutButton from 'components/CheckoutButton';
-import { useUser } from '@clerk/nextjs'
 
 export default function HomePage() {
-  const { isLoaded, isSignedIn, user } = useUser()
-  if (!isLoaded || !isSignedIn) {
-    return null
-  }
-
   return (
     <div>
       <Header />
-      <div>Hello, {user.firstName} ! welcome to Clerk</div>
-      {user.emailAddresses.map((email) => (
-        <div key={email.emailAddress}>{email.emailAddress}</div>
-      ))}
-      {user.id}
       <CheckoutButton />
     </div>
   );
 }
-
-
--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const publicRoutes = createRouteMatcher([
-  '/api/clerk-webhook(.*)',
-  // Add other specific public routes here, e.g.:
-  // '/about',
-  // '/pricing',
+  
 ])
 
 export default clerkMiddleware((auth, req) => {
